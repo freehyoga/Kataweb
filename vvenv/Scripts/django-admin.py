d4dac1ea05b9573c6bfe03ed64bcfe37dd090686
Andres Carrion <afcarrion02@gmail.com>
2018-04-02 00:28:48 -0500

    Subo la estructura del proyecto. Primer commit

---
+++ vvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\carricas\documents\platzi\django\proyectos\kataweb\vvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
